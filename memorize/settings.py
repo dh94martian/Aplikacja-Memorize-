814af82a4de57e334378269d50ac1abce3d92be2
krzyrad <krzysztof.radon@protonmail.ch>
2017-11-16 18:17:13 +0100

    Utworzenie w bazie danych tabeli dla Topic

--- memorize/settings.py
+++ memorize/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Nasze aplikacje
+    'memorizeapp',
 ]
 
 MIDDLEWARE = [
