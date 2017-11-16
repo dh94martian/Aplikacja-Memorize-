b3f53a6be721a9b44c7e2f10764905d0a99a1670
krzyrad <krzysztof.radon@protonmail.ch>
2017-11-16 23:06:23 +0100

    Utworzenie w bazie danych tabeli dla Entry

--- memorizeapp/models.py
+++ memorizeapp/models.py
@@ -6,6 +6,7 @@ class Topic(models.Model):
     """Temat tworzony przez użytkownika"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         """Zwrot w postaci ciągu tekstowego"""
         return self.text
@@ -20,4 +21,4 @@ class Entry(models.Model):
         verbose_name_plural = 'entries'
 
     def __str__(self):
-        return self.text
+        return self.text[:50] + "..."
