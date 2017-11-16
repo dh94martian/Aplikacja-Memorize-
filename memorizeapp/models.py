a7bc06256bf44b8a6bbf2aafdba8a90503580d65
Krzych12 <33066609+Krzych12@users.noreply.github.com>
2017-11-16 22:06:19 +0100

    Add files via upload
    branch update

--- memorizeapp/models.py
+++ memorizeapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    """Temat tworzony przez użytkownika"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        """Zwrot w postaci ciągu tekstowego"""
+        return self.text
+
+class Entry(models.Model):
+    """Wpis dodawany przez użytkownika"""
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    """Forma entries zamiast Entrys przy odwoływaniu się do kilku wpisów"""
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return self.text[:50] + "..."
