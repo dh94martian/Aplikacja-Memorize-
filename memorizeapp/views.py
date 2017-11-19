43869cdbc4291fe12091b222f849f39466ac532e
krzyrad <krzysztof.radon@protonmail.ch>
2017-11-19 20:47:38 +0100

    Utworzenie widoku i zbudowanie szablonu strony głównej

--- memorizeapp/views.py
+++ memorizeapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """Strona główna dla aplikacji Memorize!"""
+    return render(request, 'memorizeapp/index.html')
