a1981998c8c7033e7d25e0e379eb76f9806b8f9b
Krzych12 <33066609+Krzych12@users.noreply.github.com>
2017-11-23 23:02:07 +0100

    Add files via upload
    Branch update

--- memorizeapp/urls.py
+++ memorizeapp/urls.py
@@ -7,4 +7,10 @@ from . import views
 urlpatterns = [
     #Strona główna
     url(r'^$', views.index, name='index'),
+
+    #Wyświetlenie wszystkich tematów
+    url(r'^topics/$', views.topics, name='topics'),
+
+    #Strona szczegółowa dotycząca pojedynczego tematu
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
--- memorizeapp/views.py
+++ memorizeapp/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 # Create your views here.
 
 def index(request):
     """Strona główna dla aplikacji Memorize!"""
     return render(request, 'memorizeapp/index.html')
+
+def topics(request):
+    """Wyświetlenie wszystkich tematów"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'memorizeapp/topics.html', context)
+
+def topic(request, topic_id):
+    """Wyświetla pojedynczy temat i wszystkie powiązane z nim wpisy"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'memorizeapp/topic.html', context)
