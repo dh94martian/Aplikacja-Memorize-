5a9f4206fe92fe318c72226a99628ea3265fc2ae
krzyrad <krzysztof.radon@protonmail.ch>
2017-11-27 00:26:06 +0100

    URL dla strony edycji wpisu

--- memorizeapp/urls.py
+++ memorizeapp/urls.py
@@ -19,4 +19,7 @@ urlpatterns = [
 
     #Strona przeznaczona do dodawania nowego wpisu
     url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+
+    #Strona przeznaczona do edycji wpisu
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 ]
