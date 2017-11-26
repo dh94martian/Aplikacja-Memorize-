b6db6c072b276c0f89dcf9654491b3ebe8b71851
Krzych12 <33066609+Krzych12@users.noreply.github.com>
2017-11-26 21:37:36 +0100

    Add files via upload
    Branch update

--- memorizeapp/urls.py
+++ memorizeapp/urls.py
@@ -13,4 +13,10 @@ urlpatterns = [
 
     #Strona szczegółowa dotycząca pojedynczego tematu
     url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+
+    #Strona przeznaczona do dodawania nowego tematu
+    url(r'^new_topic/$', views.new_topic, name='new_topic'),
+    
+    #numeracja nowych wpisów
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry')
 ]
