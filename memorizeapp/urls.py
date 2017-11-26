754f40c81cef09fb52e673de054e582361348fee
krzyrad <krzysztof.radon@protonmail.ch>
2017-11-26 22:41:50 +0100

    Łącze na stronę new_entry

--- memorizeapp/urls.py
+++ memorizeapp/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
     #Strona przeznaczona do dodawania nowego tematu
     url(r'^new_topic/$', views.new_topic, name='new_topic'),
 
-    #numeracja nowych wpisów
-    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry')
+    #Strona przeznaczona do dodawania nowego wpisu
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
 ]
