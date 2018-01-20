14b42fba8d28fd22abdfa210cfc8d98e14d9c701
krzyrad <krzysztof.radon@protonmail.ch>
2018-01-20 21:57:21 +0100

    korekta ustawień hosta 2

--- memorize/settings.py
+++ memorize/settings.py
@@ -145,8 +145,8 @@ if os.getcwd() == '/app':
     ALLOWED_HOSTS = ['*']
 
     #Konfiguracja zasobów
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
+        os.path.join(PROJECT_ROOT, 'static'),
     )
